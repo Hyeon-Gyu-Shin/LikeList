5c6f42c53e1e518d5a3b27afff47f6f2d90cc8ba
Hyeon-Gyu-Shin <78663871+Hyeon-Gyu-Shin@users.noreply.github.com>
2023-12-31 23:09:02 +0900

    db완성

---
+++ src/main/java/com/example/likelist/LikeListApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/likelist/LikeListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
